2680c2efbb29e602593ce3d04ae13411a3a79c9a
Pedro Ernesto Machado <pedro.mdpn@gmail.com>
2023-10-20 06:58:47 -0300

    retoque service

--- src/main/java/io/sim/Service.java
+++ src/main/java/io/sim/Service.java
@@ -2,7 +2,6 @@ package io.sim;
 
 import de.tudresden.sumo.cmd.Route;
 import de.tudresden.sumo.cmd.Vehicle;
-import de.tudresden.sumo.objects.SumoStringList;
 import it.polito.appeal.traci.SumoTraciConnection;
 
 public class Service extends Thread {
@@ -10,17 +9,16 @@ public class Service extends Thread {
 	private String idTransportService;
 	private boolean on_off;
 	private SumoTraciConnection sumo;
-	private Auto auto;
-	private io.sim.Route route;
+	private Car car;
 
-	public Service(boolean _on_off, String _idTransportService, io.sim.Route _route, Auto _auto,
+	public Service(boolean _on_off, String _idTransportService, Car car,
 			SumoTraciConnection _sumo) {
 
 		this.on_off = _on_off;
 		this.idTransportService = _idTransportService;
-		this.route = _route;
-		this.auto = _auto;
+		this.car = car;
 		this.sumo = _sumo;
+		this.start();
 	}
 
 	@Override
@@ -28,15 +26,14 @@ public class Service extends Thread {
 		try {
 			
 			this.initializeRoutes();
-
-			this.auto.start();
+			this.car.start();
 
 			while (this.on_off) {
 				try {
-					this.sumo.do_timestep();
+					//this.sumo.do_timestep();
 				} catch (Exception e) {
 				}
-				Thread.sleep(this.auto.getAcquisitionRate());
+				Thread.sleep(this.car.getAcquisitionRate());
 				if (this.getSumo().isClosed()) {
 					this.on_off = false;
 					System.out.println("SUMO is closed...");
@@ -51,10 +48,10 @@ public class Service extends Thread {
 	private void initializeRoutes() {
 
 		try {
-			sumo.do_job_set(Route.add(this.route.getId(), this.route.getEdges()));
+			sumo.do_job_set(Route.add(this.car.getCurrenRoute().getId(), this.car.getCurrenRoute().getEdges()));
 			
-			sumo.do_job_set(Vehicle.addFull(this.auto.getIdAuto(), 				//vehID
-											this.route.getId(), 	            //routeID 
+			sumo.do_job_set(Vehicle.addFull(this.car.getIdAuto(), 				//vehID
+											this.car.getCurrenRoute().getId(), 	//routeID 
 											"DEFAULT_VEHTYPE", 					//typeID 
 											"now", 								//depart  
 											"0", 								//departLane 
@@ -66,11 +63,11 @@ public class Service extends Thread {
 											"",									//fromTaz 
 											"",									//toTaz 
 											"", 								//line 
-											this.auto.getPersonCapacity(),		//personCapacity 
-											this.auto.getPersonNumber())		//personNumber
+											this.car.getPersonCapacity(),		//personCapacity 
+											this.car.getPersonNumber())		//personNumber
 					);
 			
-			sumo.do_job_set(Vehicle.setColor(this.auto.getIdAuto(), this.auto.getColorAuto()));
+			sumo.do_job_set(Vehicle.setColor(this.car.getIdAuto(), this.car.getColorAuto()));
 			
 		} catch (Exception e1) {
 			e1.printStackTrace();
@@ -81,7 +78,7 @@ public class Service extends Thread {
 		return on_off;
 	}
 
-	public void setOn_off(boolean _on_off) {
+	public void setOn(boolean _on_off) {
 		this.on_off = _on_off;
 	}
 
@@ -93,11 +90,11 @@ public class Service extends Thread {
 		return this.sumo;
 	}
 
-	public Auto getAuto() {
-		return this.auto;
+	public Car getCar() {
+		return this.car;
 	}
 
 	public io.sim.Route getRoute() {
-		return this.route;
+		return this.car.getCurrenRoute();
 	}
 }
